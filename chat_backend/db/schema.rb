963478c9e8df7771c3bfec7426bc3ca6d27769ca
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-10-22 01:55:54 +0530

    Add blog feature

--- chat_backend/db/schema.rb
+++ chat_backend/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604064607) do
+ActiveRecord::Schema.define(version: 20181021202229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "device_tokens", force: :cascade do |t|
     t.bigint "user_id"
